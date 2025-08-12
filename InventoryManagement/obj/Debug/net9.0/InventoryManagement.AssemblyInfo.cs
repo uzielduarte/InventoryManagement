0ffaf0542c09070d7eaafac14cd4d5ff6fafae7d
Uziel Duarte <duarteuziel36@gmail.com>
2025-08-12 11:04:26 -0600

    dividing into three layers

--- InventoryManagement/obj/Debug/net9.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/obj/Debug/net9.0/InventoryManagement.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c1d93b05519649825cd4e0ca07505e6deafa280")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
