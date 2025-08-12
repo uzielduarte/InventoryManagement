bd3056f88c9e85487dd93d43495002f35e3b0abd
Uziel Duarte <duarteuziel36@gmail.com>
2025-08-12 11:28:09 -0600

    Installing nugets and adding references

--- InventoryManagement.Models/ViewModels/ErrorViewModel.cs
+++ InventoryManagement.Models/ViewModels/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-namespace InventoryManagement.Models
+namespace InventoryManagement.Models.ViewModels
 {
     public class ErrorViewModel
     {
--- InventoryManagment.DataAccess/Data/ApplicationDbContext.cs
+++ InventoryManagment.DataAccess/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace InventoryManagement.Data
+namespace InventoryManagement.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
